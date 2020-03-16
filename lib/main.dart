ea0dfed23ab636304e90a20fa63049e8332dab00
Naomi <naomi.ndibui@gmail.com>
2020-03-16 15:30:37 +0300

    added a center widgetand made text widget its child

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: Text(data),
-));
+void main() => runApp(MaterialApp(home: Center(child: Text('Hello world'))));
 
 
