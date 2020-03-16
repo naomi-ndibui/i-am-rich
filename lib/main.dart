df86dd8b3766571945efde1beba8dcfed6a59c96
Naomi <naomi.ndibui@gmail.com>
2020-03-16 15:50:34 +0300

    added an appbar widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Scaffold()
+      home: Scaffold(
+        appBar: AppBar(),
+      ),
     ),
   );
 }
