f72c3c94e0059c8e8e57bdaf75003d214ef8e16b
Naomi <naomi.ndibui@gmail.com>
2020-03-16 15:48:36 +0300

    added a scaffold widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello world'),
-      ),
+      home: Scaffold()
     ),
   );
 }
