9e1ee275394683968466cfef9c0e720a5adfc76a
Naomi <naomi.ndibui@gmail.com>
2020-03-18 12:44:47 +0300

    added a body and an image widget

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,9 @@ void main() {
           title: Text('I Am Rich'),
           backgroundColor: Colors.blueGrey[900],
         ),
+        body: Image(
+            image: 
+        ),
       ),
     ),
   );
